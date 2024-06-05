e23133e7f4dc6e017b34d6934d50807436a74698
Nugrahhadi <153716876+Nugrahhadi@users.noreply.github.com>
2024-06-05 14:44:18 +0700

    Petter Si Jenius
    upload file petter_si_jenius.c

---
+++ petter_si_jenius.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+    int arr[]={1,6,13,16,25,26};
+    int *revisi=arr;
+    
+    printf("Daftar Halaman Sebelum Perubahan:\n");
+
+    //ubah value pada array
+
+    //cetak isi array setelah perubahan
+    printf("Daftar Halaman Setelah Perubahan:\n");
+
+    return 0;
+}
